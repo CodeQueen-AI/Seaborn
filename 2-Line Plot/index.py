e24bb59315375d8bd58831871cf3c0b933a2aba1
CodeQueen-AI <codeq209@gmail.com>
2026-01-04 18:39:45 +0500

    update

--- 2-Line Plot/index.py
+++ 2-Line Plot/index.py
@@ -1,16 +1,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Sample data
 data = {
     'Month': [1, 2, 3, 4, 5, 6],
     'Sales': [200, 250, 300, 350, 400, 450]
 }
 
-# Line plot
 sns.lineplot(x='Month', y='Sales', data=data, marker='o')
 
-# Titles and labels
 plt.title("Monthly Sales Line Plot")
 plt.xlabel("Month")
 plt.ylabel("Sales")
