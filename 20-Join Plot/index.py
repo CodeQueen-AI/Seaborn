e0aa69b8df86579f2e118f93f33fe6c55bbb9e6f
CodeQueen-AI <codeq209@gmail.com>
2026-01-04 19:01:36 +0500

    update

--- 20-Join Plot/index.py
+++ 20-Join Plot/index.py
@@ -1,10 +1,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Sample dataset
 tips = sns.load_dataset("tips")
 
-# Joint plot: scatter with histograms
 sns.jointplot(x="total_bill", y="tip", data=tips, kind="scatter")
 
 plt.show()
